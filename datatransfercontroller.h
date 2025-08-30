9d4c8636caa2e3772c6da4563fb9d05b03c90793
unknown <zak2791@gmail.com>
2025-08-30 15:35:39 +0300

    30.08.25

--- datatransfercontroller.h
+++ datatransfercontroller.h
@@ -11,6 +11,15 @@ public:
 
     void changeConnection(void);
 
+    ///////////////////////////////////////////////
+    /// \brief sendData
+    /// \return
+    /// отправка данных на компьютер секретаря
+    /// _id - id базовой категории
+    /// _data - данные
+    ///////////////////////////////////////////////
+    bool sendData(int _id, QString _data);
+
 signals:
 };
 
